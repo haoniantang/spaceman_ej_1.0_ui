f8d95a1b6c12306ea76fb053bad9f11c4b80e2c2
haotangnian <15649566594@163.com>
2019-06-13 19:13:58 +0800

    comment

--- src/routes/CommentPage/CommentPage.js
+++ src/routes/CommentPage/CommentPage.js
@@ -4,9 +4,8 @@ import styles from './CommentPage.less'
 // 导入组件
 import {Modal,Button, Table,message} from 'antd'
 import axios from '../../utils/axios'
-import CustomerForm from './CustomerForm'
 
-class CommentPage extends React.CommentPage {
+class CommentPage extends React.Component {
     // 局部状态state
     constructor(){
         super();
@@ -23,9 +22,9 @@ class CommentPage extends React.CommentPage {
         this.reloadData();
     }
     // 重载数据
-    reloadData(order_id){
+    reloadData(){
         this.setState({loading:true});
-        axios.get("/customer/findAllCustomer")
+        axios.get("/comment/findCommentByOrderId?5001")
         .then((result)=>{
         // 将查询数据更新到state中
         this.setState({list:result.data})
@@ -34,96 +33,19 @@ class CommentPage extends React.CommentPage {
         this.setState({loading:false});
         })
     }
-    // 批量删除
-  handleBatchDelete(){
-    Modal.confirm({
-      title: '确定删除这些记录吗?',
-      content: '删除后数据将无法恢复',
-      onOk:() => {
-        axios.post("/customer/batchDeleteCustomer",{ids:this.state.ids})
-        .then((result)=>{
-          //批量删除后重载数据
-          message.success(result.statusText)
-          this.reloadData();
-        })
-      }
-    });
-  }
+    
 
-  // 单个删除
-  handleDelete(id){
-    Modal.confirm({
-      title: '确定删除这条记录吗?',
-      content: '删除后数据将无法恢复',
-      onOk:() => {
-        // 删除操作
-        axios.get("/customer/deleteCustomerById",{
-          params:{
-            id:id
-          }
-        })
-        .then((result)=>{
-          // 删除成功后提醒消息，并且重载数据
-          message.success(result.statusText);
-          this.reloadData();
-        })
-      }
-    });
-  }
-  // 取消按钮的事件处理函数
-  handleCancel = () => {
-    this.setState({ visible: false });
-  };
-  // 确认按钮的事件处理函数
-  handleCreate = () => {
-    const form = this.formRef.props.form;
-    form.validateFields((err, values) => {
-      if (err) {
-        return;
-      }
-      // 表单校验完成后与后台通信进行保存
-      axios.post("/customer/saveOrUpdateCustomer",values)
-      .then((result)=>{
-        message.success(result.statusText)
-        // 重置表单
-        form.resetFields();
-        // 关闭模态框
-        this.setState({ visible: false });
-        this.reloadData();
-      })
-      
-    });
-  };
-  // 将子组件的引用在父组件中进行保存，方便后期调用
-  saveFormRef = formRef => {
-    this.formRef = formRef;
-  };
-  // 去添加
-  toAdd(){
-    // 将默认值置空,模态框打开
-    this.setState({customer:{},visible:true})
-  }
-  // 去更新
-  toEdit(record){
-    // 更前先先把要更新的数据设置到state中
-    this.setState({customer:record})
-    // 将record值绑定表单中
-    this.setState({visible:true})
-  }
+
 
   // 组件类务必要重写的方法，表示页面渲染
   render(){
     // 变量定义
     let columns = [{
-      title:'姓名',
-      dataIndex:'realname'
+      title:'描述',
+      dataIndex:'content'
     },{
-      title:'手机号',
-      dataIndex:'telephone'
-    },{
-      title:'状态',
-      align:"center",
-      dataIndex:'status'
+      title:'评论时间',
+      dataIndex:'commentTime'
     },{
       title:'操作',
       width:120,
@@ -131,32 +53,19 @@ class CommentPage extends React.CommentPage {
       render:(text,record)=>{
         return (
           <div>
-            <Button type='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
-            <Button type='link' size="small" onClick={this.toEdit.bind(this,record)}>修改</Button>
+            <Button type='link' size="small" >删除</Button>
+            <Button type='link' size="small" >修改</Button>
           </div>
         )
       }
     }]
-    const rowSelection = {
-      onChange: (selectedRowKeys, selectedRows) => {
-        // 当用户操作复选按钮的时候，将值获取到并且保存到state中
-        this.setState({
-          ids:selectedRowKeys
-        })
-      },
-      getCheckboxProps: record => ({
-        disabled: record.name === 'Disabled User', // Column configuration not to be checked
-        name: record.name,
-      }),
-    };
     
     // 返回结果 jsx(js + xml)
     return (
-      <div className={styles.customer}>
+      <div className={styles.comment}>
         <div className={styles.title}>顾客管理</div>
         <div className={styles.btns}>
-          <Button onClick={this.toAdd.bind(this)}>添加</Button> &nbsp;
-          <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button> &nbsp;
+          
           <Button type="link">导出</Button>
         </div>
         <Table 
@@ -164,16 +73,8 @@ class CommentPage extends React.CommentPage {
           rowKey="id"
           size="small"
           loading={this.state.loading}
-          rowSelection={rowSelection}
           columns={columns}
           dataSource={this.state.list}/>
-
-        <CustomerForm
-          initData={this.state.customer}
-          wrappedComponentRef={this.saveFormRef}
-          visible={this.state.visible}
-          onCancel={this.handleCancel}
-          onCreate={this.handleCreate}/>
       </div>
     )
   }
