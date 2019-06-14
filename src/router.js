1ccc3515e03c858b1a67591ffd1b7c92c70936dd
haotangnian <15649566594@163.com>
2019-06-14 15:10:22 +0800

    导航栏

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch, Link } from 'dva/router';
 import CustomerPage from './routes/CustomerPage/CustomerPage';
 import CommentPage from './routes/CommentPage/CommentPage';
 import OrderPage from './routes/OrderPage/OrderPage';
@@ -9,21 +9,42 @@ import WaiterPage from './routes/WaiterPage/WaiterPage';
 import LoginPage from './routes/LoginPage/LoginPage';
 import AddressPage from './routes/AddressPage/AddressPage';
 import IndexPage from './routes/IndexPage/IndexPage';
+import styles from './router.css'
+//import newIndexPage from './routes/IndexPage/newIndexPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Switch>
-        <Route path = "/" exact component = {IndexPage} />
-        <Route path = "/customer" exact component = {CustomerPage} />
-        <Route path = "/comment" exact component = {CommentPage} />
-        <Route path = "/order" exact component = {OrderPage} />
-        <Route path = "/product" exact component = {ProductPage} />
-        <Route path = "/category" exact component = {CategoryPage} />
-        <Route path = "/waiter" exact component = {WaiterPage} />
+      <div className={styles.container}>
+          <div className={styles["left-nav"]}>
+            <div className={styles.title}>E洁家政管理系统</div>
+            <ul>
+              <li className={styles["nav-list-item"]}><Link to="/customer">顾客管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/order">订单管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/address">地址管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/comment">评论管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/product">产品管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/category">分类管理</Link></li>
+              <li className={styles["nav-list-item"]}><Link to="/waiter">服务员管理</Link></li>
+            </ul>
+          </div>
+
+          <div className={styles["right-content"]}>
+            <Switch>
+              <Route path="/" exact component={IndexPage} />
+              <Route path="/customer" exact component={CustomerPage} />
+              <Route path="/order" exact component={OrderPage} />
+              <Route path="/address" exact component={AddressPage} />
+              <Route path = "/comment" exact component = {CommentPage} />
+              <Route path = "/product" exact component = {ProductPage} />
+              <Route path = "/category" exact component = {CategoryPage} />
+              <Route path = "/waiter" exact component = {WaiterPage} />
+            </Switch>
+          </div>
+        </div>
+      {/* <Switch>
         <Route path = "/login" exact component ={LoginPage} />
-        <Route path = "/address" exact component ={AddressPage} />
-      </Switch>
+      </Switch> */}
     </Router>
   );
 }
