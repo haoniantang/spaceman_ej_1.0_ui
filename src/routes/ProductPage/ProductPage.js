dbddda582a1187c15e0e2e35c026195cb5329176
Spaceman <mirrors@live.cn>
2019-06-17 18:07:20 +0800

    添加新建产品与图片上传

--- src/routes/ProductPage/ProductPage.js
+++ src/routes/ProductPage/ProductPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './ProductPage.less';
 import axios from '../../utils/axios'
-import {Table,Button,Input,Form,message} from 'antd'
+import {Modal ,Table,Button,Input,Form,message} from 'antd'
+import ProductForm from './ProductForm'
 const FormItem = Form.Item;
 
 class ProductPage extends React.Component {
@@ -11,7 +12,9 @@ class ProductPage extends React.Component {
     this.state = {
       ids:[],
       list:[],
-      loading:false
+      loading:false,
+      visible:false,
+      product:{}
     }
 }
 
@@ -33,7 +36,81 @@ reloadData(){
         this.setState({loading:false});
     })
 }
-
+handleBatchDelete(){
+  Modal.confirm({
+    title: '确定删除这些记录吗?',
+    content: '删除后数据将无法恢复',
+    onOk:() => {
+      axios.post("/product/deleteBathProduct",{ids:this.state.ids})
+      .then((result)=>{
+        //批量删除后重载数据
+        message.success(result.statusText)
+        this.reloadData();
+      })
+    }
+  });
+}
+//单个删除
+handleDelete(id){
+  Modal.confirm({
+    title: '确定删除这条记录吗?',
+    content: '删除后数据将无法恢复',
+    onOk:() => {
+      // 删除操作
+      axios.get("/product/deleteProductById",{
+        params:{
+          id:id
+        }
+      })
+      .then((result)=>{
+        // 删除成功后提醒消息，并且重载数据
+        message.success(result.statusText);
+        this.reloadData();
+      })
+    }
+  });
+}
+// 取消按钮的事件处理函数
+handleCancel = () => {
+  this.setState({ visible: false });
+};
+// 确认按钮的事件处理函数
+handleCreate = () => {
+  const form = this.formRef.props.form;
+  form.validateFields((err, values) => {
+    if (err) {
+      return;
+    }
+    alert(JSON.stringify(values));
+    // 表单校验完成后与后台通信进行保存
+    axios.post("/product/updateProduct",values)
+    .then((result)=>{
+      message.success(result.statusText)
+      // 重置表单
+      form.resetFields();
+      // 关闭模态框
+      this.setState({ visible: false });
+      this.reloadData();
+    })
+    
+  });
+};
+// 将子组件的引用在父组件中进行保存，方便后期调用
+saveFormRef = formRef => {
+  this.formRef = formRef;
+};
+// 去添加
+toAdd(){
+  // 将默认值置空,模态框打开
+  this.setState({product:{},visible:true})
+}
+// 去更新
+toEdit(record){
+  // 更前先先把要更新的数据设置到state中
+  this.setState({product:record})
+  // 将record值绑定表单中
+  this.setState({visible:true})
+}
 //加载查询的服务
 handleSearch = ()=>{
   let product = this.props.form.getFieldsValue();
@@ -51,7 +128,13 @@ sendId=()=>{
     this.setState({list:result.data})
   })
 }
-
+//更新
+toEdit(record){
+  // 更前先先把要更新的数据设置到state中
+  this.setState({product:record})
+  // 将record值绑定表单中
+  this.setState({visible:true})
+}
 //返回成功创建订单成功的信息
 Success=()=>{
   message.success("订单创建成功！")
@@ -80,7 +163,28 @@ Success=()=>{
       title:"服务概览",
       dataIndex:"photo",
       width:"30%"
-  },];
+  },{
+    title: '图片',
+    align: 'center',
+    dataIndex: 'photo',
+    render(text){
+      return (
+        <img width={40} height={40} src={"http://134.175.154.93:8888/group1/"+text}/>
+      )
+    }
+  },{
+    title:'操作',
+      width:160,
+      align:"center",
+      render:(text,record)=>{
+        return (
+          <div>
+            <Button type='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
+            <Button type='link' size="small" onClick={this.toEdit.bind(this,record)}>修改</Button>
+          </div>
+        )
+      }
+  }];
 
 // 当用户操作复选按钮的时候，将值获取到并且保存到state中
   const rowSelection = {
@@ -96,29 +200,31 @@ Success=()=>{
     return (
       <div className={styles.product}>
       <div className={styles.title}>服务选择</div>
-                      <Form>
-                        <FormItem>
-                            {
-                                getFieldDecorator('productName',{
-                                    initialValue:'',
-                                    rules:[
-                                        {
-                                            required:true,
-                                            message:'输入不能为空'
-                                        }
-                                    ]
-                                })(
-                                    <Input className={styles.Input} placeholder=" 请查询服务名称" />
-                                )
-                            }
-                        </FormItem>
-                        <FormItem className={styles.additional}>
-                            <Button type="primary" htmlType="submit" onClick={this.handleSearch}>查询</Button>
-                            &nbsp;&nbsp;
-                            <Button type="primary" onClick={this.reloadData}>所有服务</Button>
-                            <Button type="primary" onClick={this.Success} onClick={this.sendId} className={styles.selSerBtn}>选择服务</Button>
-                        </FormItem>
-                       </Form> 
+        <Form>
+          <FormItem>
+              {
+                  getFieldDecorator('productName',{
+                      initialValue:'',
+                      rules:[
+                          {
+                            required:true,
+                              message:'输入不能为空'
+                          }
+                      ]
+                  })(
+                      <Input className={styles.Input} placeholder=" 请查询服务名称" />
+                  )
+              }
+          </FormItem>
+          <FormItem className={styles.additional}>
+              <Button type="primary" htmlType="submit" onClick={this.handleSearch}>查询</Button>
+              &nbsp;&nbsp;
+              <Button type="primary" onClick={this.reloadData}>所有服务</Button>
+              &nbsp;&nbsp;
+              <Button type="primary" onClick={this.toAdd.bind(this)}>添加</Button> &nbsp;
+              <Button type="primary" onClick={this.Success} onClick={this.sendId} className={styles.selSerBtn}>选择服务</Button>
+          </FormItem>
+          </Form> 
         <Table 
           bordered
           rowKey="id"
@@ -127,6 +233,13 @@ Success=()=>{
           rowSelection={rowSelection}
           columns={columns}
           dataSource={this.state.list}/>
+
+        <ProductForm
+          initData={this.state.product}
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}/>
       </div>
   )
   }
